834a69f772630333b4edd02366552119283eccf2
alessandro <alebeni02@gmail.com>
2021-03-08 16:00:13 +0100

    fine prima parte

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 var parola = prompt("Inserisci una parola");
 var p_invertita = "";
-for (i=parola.length - 1; i>=0 ; i--) {
-  var inverto = parola[i].charAt(i);
+for (i = parola.length - 1; i>=0 ; i--) {
   document.write(parola.charAt(i));
-  p_invertita += inverto;
+  p_invertita += parola.charAt(i);
 }
-if (parola = p_invertita) {
+if (parola === p_invertita) {
   document.write(" La parola é polindroma");
 } else {
   document.write(" La parola non é polindroma");
