fdd015a98d97a05e9cda271f0a8c90fa3eaf2817
alessandro <alebeni02@gmail.com>
2021-03-08 17:29:53 +0100

    fatta la prima funzione

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 var parola = prompt("Inserisci una parola");
 var p_invertita = "";
-for (i = parola.length - 1; i>=0 ; i--) {
-  document.write(parola.charAt(i));
-  p_invertita += parola.charAt(i);
+inverti(parola)
+function inverti(parola) {
+  for (i = parola.length - 1; i>=0 ; i--) {
+    document.write(parola.charAt(i));
+    p_invertita += parola.charAt(i);
+  }
+  if (parola === p_invertita) {
+    document.write(" La parola é polindroma");
+  } else {
+    document.write(" La parola non é polindroma");
+  }
+  return p_invertita;
 }
-if (parola === p_invertita) {
-  document.write(" La parola é polindroma");
-} else {
-  document.write(" La parola non é polindroma");
-}
+
+
+// var scegli = prompt("Pari o Dispari?");
+// var numero_utente = parseInt(prompt("Scegli un numero da 1 a 5"));
