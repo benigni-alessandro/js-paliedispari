9ec5009289c30078fdf01761411c377f5f6486dc
alessandro <alebeni02@gmail.com>
2021-03-08 20:15:09 +0100

    .

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-// var parola = prompt("Inserisci una parola");
-// var p_invertita = "";
-// inverti(parola)
-// function inverti(parola) {
-//   for (i = parola.length - 1; i>=0 ; i--) {
-//     document.write(parola.charAt(i));
-//     p_invertita += parola.charAt(i);
-//   }
-//   if (parola === p_invertita) {
-//     document.write(" La parola é polindroma");
-//   } else {
-//     document.write(" La parola non é polindroma");
-//   }
-//   return p_invertita;
-// }
+var parola = prompt("Inserisci una parola");
+var p_invertita = "";
+inverti(parola)
+function inverti(parola) {
+  for (i = parola.length - 1; i>=0 ; i--) {
+    document.write(parola.charAt(i));
+    p_invertita += parola.charAt(i);
+  }
+  if (parola === p_invertita) {
+    document.write(" La parola é polindroma");
+  } else {
+    document.write(" La parola non é polindroma");
+  }
+  return p_invertita;
+}
 
 
 var scegli = prompt("pari o dispari?");
